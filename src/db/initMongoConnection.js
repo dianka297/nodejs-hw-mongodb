654b7cc1a1789e2573f07ab77d58be9d8335b326
dianka297 <dianamakharadze96@gmail.com>
2025-03-20 17:39:59 +0400

    hw done

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,7 +6,8 @@ const pswd = getEnvVar('MONGODB_PASSWORD');
 const url = getEnvVar('MONGODB_URL');
 const db = getEnvVar('MONGODB_DB');
 
-const DB_URI = `mongodb+srv://${user}:${pswd}@${url}/${db}?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://dianamakharadze96:01-E729D@cluster.jryok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+
 
 export async function initMongoConnection() {
     try{
