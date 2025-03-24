5b4cc6661954877096187b5c6cada03fd64e04af
dianka297 <dianamakharadze96@gmail.com>
2025-03-24 11:22:28 +0400

    fix mistakes

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,7 +6,8 @@ const pswd = getEnvVar('MONGODB_PASSWORD');
 const url = getEnvVar('MONGODB_URL');
 const db = getEnvVar('MONGODB_DB');
 
-const DB_URI = `mongodb+srv://${user}:${pswd}@${url}/${db}?retryWrites=true&w=majority`;
+const DB_URI = `mongodb+srv://${user}:${pswd}@cluster0.xxxxx.mongodb.net/${db}?retryWrites=true&w=majority`;
+
 
 export async function initMongoConnection() {
     try{
