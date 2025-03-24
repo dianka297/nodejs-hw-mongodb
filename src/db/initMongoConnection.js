43fc21209be5f5a9808e1f266784212dd6cc1600
dianka297 <dianamakharadze96@gmail.com>
2025-03-24 11:58:16 +0400

    add fix mistakes

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,21 +1,18 @@
 import mongoose from "mongoose";
 import { getEnvVar } from "../utils/getEnvVar.js";
 
-const user = getEnvVar('MONGODB_USER');
-const pswd = getEnvVar('MONGODB_PASSWORD');
-const url = getEnvVar('MONGODB_URL');
-const db = getEnvVar('MONGODB_DB');
+const user = getEnvVar("MONGODB_USER");
+const pswd = getEnvVar("MONGODB_PASSWORD");
+const db = getEnvVar("MONGODB_DB");
 
-const uri = "mongodb+srv://dianamakharadze96:01-E729D@cluster.jryok.mongodb.net/Contacts?retryWrites=true&w=majority";
-
-
+const uri = `mongodb+srv://${user}:${pswd}@cluster.jryok.mongodb.net/${db}?retryWrites=true&w=majority`;
 
 export async function initMongoConnection() {
-    try{
-        await mongoose.connect(DB_URI);
-        console.log("Mongo connection successfully established!");
-    }catch(error){
-        console.error("Mongodb connection error",error);
-        process.exit(1);
-    } 
+  try {
+    await mongoose.connect(uri);
+    console.log("✅ Mongo connection successfully established!");
+  } catch (error) {
+    console.error("❌ Mongodb connection error", error);
+    process.exit(1);
+  }
 }
