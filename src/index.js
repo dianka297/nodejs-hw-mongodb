aab9cc0d143e2bbaa0c1259c1ceb2edabb8aa353
dianka297 <dianamakharadze96@gmail.com>
2025-05-07 17:16:45 +0400

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import initMongoConnection from './db/initMongoConnection.js';
-import setupServer from './server.js';
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
-};
+// Подключение к MongoDB, затем запуск сервера
+async function main() {
+  await initMongoConnection(); // Установим соединение с MongoDB
+  setupServer();               // Запускаем сервер Express
+}
 
-bootstrap();
+main();
