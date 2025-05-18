cb3c09e7033f941d043cf3bb6b94777d0d4b2976
dianka297 <dianamakharadze96@gmail.com>
2025-05-18 20:50:22 +0400

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { initMongoConnection } from "./db/initMongoConnection.js";
-import { setupServer } from "./server.js";
+import setupServer from "./server.js";
 
 async function startApp() {
     await initMongoConnection();
     setupServer();
 }
 
-startApp(); // ✅ Викликаємо правильну функцію
+startApp();
