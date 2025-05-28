543c6b03bf871f91d32143a0493c322b6610bd44
dianka297 <dianamakharadze96@gmail.com>
2025-05-28 21:50:13 +0400

    Fix import: use Contact instead of ContactsCollection

--- src/middlewares/checkContactAccess.js
+++ src/middlewares/checkContactAccess.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { ContactsCollection } from '../db/models/contact.js';
+import { Contact } from '../db/models/contact.js';
 
 export const checkContactAccess = async (req, res, next) => {
   const { user } = req;
