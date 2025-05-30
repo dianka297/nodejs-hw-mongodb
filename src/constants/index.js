f7c4a6472786bfa9f1f2217a641680a107797d1e
dianka297 <dianamakharadze96@gmail.com>
2025-05-30 23:45:16 +0400

    Force refresh Render build

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-export const SORT_ORDER = {
-    ASC: 'asc',
-    DESC: 'desc',
-  };
-  export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-  export const ONE_DAY = 24 * 60 * 60 * 1000;
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const ACCESS_SECRET = process.env.ACCESS_SECRET || 'default-access-secret';
+export const REFRESH_SECRET = process.env.REFRESH_SECRET || 'default-refresh-secret';
+export const ONE_DAY = 24 * 60 * 60 * 1000;
