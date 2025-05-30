565a5eaafb91de79b3ecdee8afed20058ad88ab5
dianka297 <dianamakharadze96@gmail.com>
2025-05-30 23:40:07 +0400

    Fix: add ACCESS_SECRET and REFRESH_SECRET to constants

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { initMongoConnection } from "./db/initMongoConnection.js";
-import setupServer from "./server.js";
+import dotenv from 'dotenv';
+dotenv.config(); // обов'язково перед іншими імпортами
+
+import { initMongoConnection } from './db/initMongoConnection.js';
+import setupServer from './server.js';
 
 async function startApp() {
-    await initMongoConnection();
-    setupServer();
+  await initMongoConnection();
+  setupServer();
 }
 
 startApp();
+
