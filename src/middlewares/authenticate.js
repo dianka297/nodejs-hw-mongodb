f4748b666936c238a545437b4493d4fe88d9fd50
dianka297 <dianamakharadze96@gmail.com>
2025-05-31 09:02:23 +0400

    Fix: replace UsersCollection with User in authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
 import { Session } from '../db/models/session.js';
-import { UsersCollection } from '../db/models/user.js';
+import { User } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
