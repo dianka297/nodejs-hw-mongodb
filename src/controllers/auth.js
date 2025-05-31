80ec8b7ee94b8fb65914dde63ccccca122268832
dianka297 <dianamakharadze96@gmail.com>
2025-05-31 09:12:22 +0400

    Finalize: working auth router and controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,62 +1,86 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { User } from '../db/models/user.js'; 
-import { Session } from '../db/models/session.js'; 
 import createHttpError from 'http-errors';
-import { ACCESS_SECRET, REFRESH_SECRET } from '../constants/index.js';
+import { registerUser, loginUser, logoutUser, refreshUsersSession } from '../services/auth.js';
+import { ONE_DAY } from '../constants/index.js';
 
-export const registerUser = async ({ email, password }) => {
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    throw createHttpError(409, 'Email in use');
+export const registerUserController = async (req, res, next) => {
+  try {
+    const user = await registerUser(req.body);
+
+    res.status(201).json({
+      status: 201,
+      message: 'Successfully registered a user!',
+      data: user,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ email, password: hashedPassword });
-
-  return { email: user.email, id: user._id };
 };
 
-export const loginUser = async ({ email, password }) => {
-  const user = await User.findOne({ email });
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    throw createHttpError(401, 'Invalid email or password');
+export const loginUserController = async (req, res, next) => {
+  try {
+    const session = await loginUser(req.body);
+
+    res.cookie('refreshToken', session.refreshToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + ONE_DAY),
+    });
+    res.cookie('sessionId', session._id, {
+      httpOnly: true,
+      expires: new Date(Date.now() + ONE_DAY),
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully logged in!',
+      data: {
+        accessToken: session.accessToken,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const accessToken = jwt.sign({ userId: user._id }, ACCESS_SECRET, { expiresIn: '1h' });
-  const refreshToken = jwt.sign({ userId: user._id }, REFRESH_SECRET, { expiresIn: '1d' });
-
-  const session = await Session.create({ user: user._id, token: refreshToken });
-
-  return {
-    accessToken,
-    refreshToken,
-    _id: session._id,
-  };
 };
 
-export const logoutUser = async (sessionId) => {
-  await Session.findByIdAndDelete(sessionId);
+export const logoutUserController = async (req, res, next) => {
+  try {
+    if (req.cookies.sessionId) {
+      await logoutUser(req.cookies.sessionId);
+    }
+
+    res.clearCookie('sessionId');
+    res.clearCookie('refreshToken');
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
-  const session = await Session.findById(sessionId);
-  if (!session || session.token !== refreshToken) {
-    throw createHttpError(401, 'Invalid session');
+export const refreshUserSessionController = async (req, res, next) => {
+  try {
+    const session = await refreshUsersSession({
+      sessionId: req.cookies.sessionId,
+      refreshToken: req.cookies.refreshToken,
+    });
+
+    res.cookie('refreshToken', session.refreshToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + ONE_DAY),
+    });
+    res.cookie('sessionId', session._id, {
+      httpOnly: true,
+      expires: new Date(Date.now() + ONE_DAY),
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully refreshed session!',
+      data: {
+        accessToken: session.accessToken,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const decoded = jwt.verify(refreshToken, REFRESH_SECRET);
-
-  const newAccessToken = jwt.sign({ userId: decoded.userId }, ACCESS_SECRET, { expiresIn: '1h' });
-  const newRefreshToken = jwt.sign({ userId: decoded.userId }, REFRESH_SECRET, { expiresIn: '1d' });
-
-  session.token = newRefreshToken;
-  await session.save();
-
-  return {
-    accessToken: newAccessToken,
-    refreshToken: newRefreshToken,
-    _id: session._id,
-  };
 };
 
