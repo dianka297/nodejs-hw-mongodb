76f39c609ec596ff3f6766bbd5a2fa3588798d13
dianka297 <dianamakharadze96@gmail.com>
2025-05-31 09:16:08 +0400

    Fix: add FIFTEEN_MINUTES to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,9 @@ dotenv.config();
 
 export const ACCESS_SECRET = process.env.ACCESS_SECRET || 'access-secret';
 export const REFRESH_SECRET = process.env.REFRESH_SECRET || 'refresh-secret';
+
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 
 export const SORT_ORDER = {
   ASC: 'asc',
