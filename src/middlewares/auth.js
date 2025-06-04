f8162852fa95f78f3c40d37cc66338e8c2c44847
dianka297 <dianamakharadze96@gmail.com>
2025-06-04 18:16:19 +0400

    Add missing auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import createHttpError from 'http-errors';
+import { User } from '../db/models/user.js';
+
+const { SECRET_KEY } = process.env;
+
+const auth = async (req, res, next) => {
+  const { authorization = '' } = req.headers;
+  const [bearer, token] = authorization.split(' ');
+
+  if (bearer !== 'Bearer' || !token) {
+    return next(createHttpError(401, 'Not authorized'));
+  }
+
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user || user.token !== token) {
+      return next(createHttpError(401, 'Not authorized'));
+    }
+
+    req.user = user;
+    next();
+  } catch {
+    next(createHttpError(401, 'Not authorized'));
+  }
+};
+
+export default auth;
