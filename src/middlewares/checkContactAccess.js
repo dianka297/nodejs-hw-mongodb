17ebbd40e62c7e0da7856a283f6bb38ebe4d9392
dianka297 <dianamakharadze96@gmail.com>
2025-06-05 14:58:57 +0400

    Fix: rename contacts.js to contact.js for model resolution

--- src/middlewares/checkContactAccess.js
+++ src/middlewares/checkContactAccess.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { Contact } from '../db/models/contacts.js';
+import { Contact } from '../db/models/contact.js';
 
 export const checkContactAccess = async (req, res, next) => {
   const { user } = req;
