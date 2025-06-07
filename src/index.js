4aae233a3ca51ebd8d3360d6c2a784a7f058978b
dianka297 <dianamakharadze96@gmail.com>
2025-06-07 15:27:09 +0400

    fix: server setup complete and import aligned

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 import { initMongoConnection } from './db/initMongoConnection.js';
-import setupServer from './server.js';
+import { setupServer } from './server.js'; // <-- Виправлено
 
 async function startApp() {
   await initMongoConnection();
