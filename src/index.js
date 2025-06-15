14be9d512d9c2319b81c47cc313898c106003f0d
dianka297 <dianamakharadze96@gmail.com>
2025-06-15 22:08:16 +0400

    fix mistakes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js'; // <-- Виправлено
+import { setupServer } from './server.js'; 
 
 async function startApp() {
   await initMongoConnection();
