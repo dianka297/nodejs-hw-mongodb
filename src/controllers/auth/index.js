f50fb8310c6b45930defe5f4f59bae3aec0f4db8
dianka297 <dianamakharadze96@gmail.com>
2025-07-03 17:55:25 +0400

    fix mistakes

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,15 +1,8 @@
-// Ре-експортуємо всі контролери, щоб роутер міг імпортувати їх
-// одним рядком  ../controllers/auth/index.js
-// ---------------------------------------------------------------
+// src/controllers/auth/index.js
+export { registerUserController }   from './registerUser.js';
+export { loginUserController }      from './loginUser.js';
+export { logoutUserController }     from './logoutUser.js';
+export { refreshSessionController } from './refreshSession.js';
 
-/* базові (реєстрація / логін / логаут / refresh) */
-export {
-  registerUserController,
-  loginUserController,
-  logoutUserController,
-  refreshSessionController,
-} from '../auth.js';          // ←  файл із базовими контролерами
-
-/* HW-6: e-mail із токеном та скидання пароля */
 export { sendResetEmailCtrl } from './sendResetEmail.js';
 export { resetPasswordCtrl }  from './resetPassword.js';
