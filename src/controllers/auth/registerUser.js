bc15282570690b76dab021b855a21f7948a9c9bd
dianka297 <dianamakharadze96@gmail.com>
2025-07-03 19:35:18 +0400

    fix mistakes

--- src/controllers/auth/registerUser.js
+++ src/controllers/auth/registerUser.js
@@ -3,16 +3,22 @@ import httpError from 'http-errors';
 import bcrypt from 'bcryptjs';
 
 export const registerUserController = async (req, res) => {
-  const { email, password } = req.body;
-  const existingUser = await User.findOne({ email });
+  const { name, email, password } = req.body; // Додали name
 
+  const existingUser = await User.findOne({ email });
   if (existingUser) throw httpError(409, 'Email already in use');
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ email, password: hashedPassword });
+
+  // Додали name при створенні
+  const newUser = await User.create({ name, email, password: hashedPassword });
 
   res.status(201).json({
     message: 'Registration successful',
-    user: { id: newUser._id, email: newUser.email },
+    user: {
+      id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+    },
   });
 };
