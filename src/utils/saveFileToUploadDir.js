0d41a7274213c45b42820852b854f92b133456ef
dianka297 <dianamakharadze96@gmail.com>
2025-07-11 23:46:16 +0400

    fix mistakes

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -3,11 +3,27 @@ import fs from 'node:fs/promises';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../constants/index.js';
 import { getEnvVar } from './getEnvVar.js';
 
-export const saveFileToUploadDir = async (file) => {
-  await fs.rename(
-    path.join(TEMP_UPLOAD_DIR, file.filename),
-    path.join(UPLOAD_DIR, file.filename),
-  );
+/**
+ * Убедись, что папка `UPLOAD_DIR` существует
+ */
+const createDirIfNotExists = async (dir) => {
+  try {
+    await fs.access(dir);
+  } catch {
+    await fs.mkdir(dir, { recursive: true });
+  }
+};
 
+export const saveFileToUploadDir = async (file) => {
+  const tempPath = path.join(TEMP_UPLOAD_DIR, file.filename);
+  const uploadPath = path.join(UPLOAD_DIR, file.filename);
+
+  // ✅ Создаём папку `public/uploads`, если её нет
+  await createDirIfNotExists(UPLOAD_DIR);
+
+  // ✅ Перемещаем файл
+  await fs.rename(tempPath, uploadPath);
+
+  // ✅ Отдаём публичный путь
   return `${getEnvVar('APP_DOMAIN')}/uploads/${file.filename}`;
 };
