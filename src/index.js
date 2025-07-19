f701d9d51a1bbd9eca0ce4bc0f41a9684cb7d60e
dianka297 <dianamakharadze96@gmail.com>
2025-07-19 15:28:47 +0400

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { setupServer } from './server.js';
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoConnection, initMongoDB } from './db/initMongoConnection.js';
 
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 import createDirIfNotExists from './utils/createDirIFNotExists.js';
 
 const bootstrap = async () => {
-  await initMongoDB();
+  await initMongoConnection();
   setupServer();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
