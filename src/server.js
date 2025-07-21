529350469cd68daed5f3e7f3eec74f322f8060cb
dianka297 <dianamakharadze96@gmail.com>
2025-07-21 14:43:56 +0400

    added swagger

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import logger from './middlewares/logger.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import swaggerDocs from './middlewares/swaggerDocs.js';  
 
 export const setupServer = () => {
   const app = express();
@@ -17,6 +18,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   //routes
   app.use('/auth', authRouter);
